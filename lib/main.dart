4a1b4b261ebc7517b1852d772a79d570059b2097
stsysd <000stsysd000@gmail.com>
2024-10-06 04:03:04 +0900

    add sample hotkey

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+import 'package:process_run/shell.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // for hot reload
+  await hotKeyManager.unregisterAll();
+
   runApp(const MainApp());
 }
 
@@ -17,4 +25,22 @@ class MainApp extends StatelessWidget {
       ),
     );
   }
+}
+
+void registerHotKeys() async {
+  await hotKeyManager.register(
+    HotKey(
+      key: PhysicalKeyboardKey.f24,
+      modifiers: [
+        HotKeyModifier.control,
+        HotKeyModifier.shift,
+        HotKeyModifier.alt,
+        HotKeyModifier.meta,
+      ],
+    ),
+    keyUpHandler: (hotkey) async {
+      var shell = Shell();
+      await shell.run('chrome.exe');
+    },
+  );
 }
